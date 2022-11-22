a5791ad68042274716eee61b4ec9465815da6cbb
TalysonSoares <talysonsoares40@gmail.com>
2022-11-22 09:34:45 -0300

    criando erro

--- public/index.php
+++ public/index.php
@@ -4,7 +4,10 @@ require_once '../src/Controller/AlunoController.php';
 
 $rotas = require '../config/routes.php';
 
-explode('?', '/excluir?id=10');
-
 $url = $_SERVER['REQUEST_URI']; //pegando a url acessada pelo usuario
 $rota =  explode('?', $url)[0]; //separando a url, atraves do "?"
+
+if (false === isset($rotas[$rota])) {
+    echo "Erro 404";
+    exit;
+}
