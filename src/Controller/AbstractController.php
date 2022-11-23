9f3417b8cd6baf7525f0a05049021c931d791d7c
TalysonSoares <talysonsoares40@gmail.com>
2022-11-23 19:07:46 -0300

    refinando forma de fazer a função renderizar

---
+++ src/Controller/AbstractController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+abstract class AbstractController
+{
+    public function render(string $view, array $dados = []): void
+    {
+        include_once '../views/template/header.phtml';
+
+        include_once '../views/template/menu.phtml';
+
+        include_once "../views/{$view}.phtml";
+
+        include_once '../views/template/footer.phtml';
+    }
+}
