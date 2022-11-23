d30ed2e6abc2eb0ea8d1900673160c984b40ee57
TalysonSoares <talysonsoares40@gmail.com>
2022-11-23 19:03:28 -0300

    atualizando index

--- public/index.php
+++ public/index.php
@@ -1,9 +1,21 @@
 <?php
 
+//solicitando ao composer que gerencie o carregamento automagico dos arquivos
+
+use App\Connection\DatabaseConnection;
+
+include_once '../vendor/autoload.php';
+
+include '../config/database.php';
+
+var_dump(
+    DatabaseConnection::abrirConexao()
+);
+
 $rotas = require '../config/routes.php';
 
 $url = $_SERVER['REQUEST_URI']; //pegando a url acessada pelo usuario
-$rota =  explode('?', $url)[0]; //separando a url, atraves do "?"
+$rota = explode('?', $url)[0]; //separando a url, atraves do "?"
 
 if (false === isset($rotas[$rota])) {
     echo "Erro 404";
@@ -14,5 +26,3 @@ $controller = $rotas[$rota]['controller'];
 $method = $rotas[$rota]['method'];
 
 (new $controller)->$method();
-
-//php -S localhost:8000 -t public
