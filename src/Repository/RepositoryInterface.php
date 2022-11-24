a941486eee42a8bbea1b5b85457170d11260c26d
TalysonSoares <talysonsoares40@gmail.com>
2022-11-24 08:45:54 -0300

    interface

---
+++ src/Repository/RepositoryInterface.php
@@ -0,0 +1,18 @@
+<?
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+interface RepositoryInterface
+{
+    public function buscarTodos(): iterable;
+
+    public function buscarUm(): ?object;
+
+    public function inserir(object $dados): object;
+
+    public function atualizar(object $dados, string $id): object;
+
+    public function excluir(string $id): void;
+}
